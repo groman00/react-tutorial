933ec93e7450480b6798068bced29a77466a4b4d
Roman, Greg <greg.roman@teamaol.com>
2016-04-09 17:02:45 -0400

    Learning react js.  Using express framework.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    rename = require('gulp-rename');
+
+var buildRoot = './public',
+    assetRoot = './src';
+
+/*
+ * JavaScript
+ */
+
+gulp.task('js', function () {
+    gulp.src(assetRoot + '/js/main.js')
+        .pipe(browserify({
+            debug: true,
+            transform: ['reactify']
+        }))
+        .pipe(rename('bundle.js'))
+        .pipe(gulp.dest(buildRoot + '/javascripts/'));
+});
+
+gulp.task('default', ['js']);
+
+gulp.task('watch', function(){
+    gulp.watch(assetRoot + '/js/**', ['js']);
+});
+
