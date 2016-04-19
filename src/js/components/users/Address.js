fefc1dd86b82c275a93dd70e2997b770ab752db9
Roman, Greg <greg.roman@teamaol.com>
2016-04-19 09:30:51 -0400

    better object iteration

--- src/js/components/users/Address.js
+++ src/js/components/users/Address.js
@@ -4,13 +4,11 @@ module.exports = React.createClass({
   render: function() {
     var data = this.props.data;
 
-    //This is most likey the wrong way render object keys in react
-    var fieldNodes = [];
-    for(var key in data){
+    var fieldNodes = Object.keys(data).map(function(key, i, obj) {
       if(typeof data[key] !== 'object') {
-        fieldNodes.push(<li key={key}>{key}:{data[key]}</li>);
+        return <li key={key}>{key}: {data[key]}</li>;
       }
-    };
+    });
 
     return (
       <ul className="collapse">
